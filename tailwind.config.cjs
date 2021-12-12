0e71817ed76ba2474ee7d5d41f29c43dca4adbe8
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-12-12 07:34:53 +0200

    chore: upgrade to tailwind 3 & add autoprefixer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
+  content: [
+    './public/**/*.html',
+    './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
+  ],
   theme: {
     variants: {
       extend: {
