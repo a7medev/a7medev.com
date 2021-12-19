e26d49a368843dadf207da9e5e04a34a5b2733cf
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-12-19 11:59:23 +0200

    chore: remove Inter font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,22 @@ module.exports = {
       },
     },
     fontFamily: {
-      sans: ['Inter', 'system-ui', 'sans-serif'],
+      sans: [
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
       serif: ['DM Serif Display', 'serif'],
     },
     colors: {
